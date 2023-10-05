5bc882612701877c1229f3604dccc657406faa83
alex-lerch <106268256+alex-lerch@users.noreply.github.com>
2023-10-04 19:09:22 -0500

    added functionality to print whether the array is sorted or not to TestSorts.cpp

--- TestSorts.cpp
+++ TestSorts.cpp
@@ -4,11 +4,16 @@
 #include "Sorts.h"
 
 void printNums(int array[], int arraySize);
+bool isSorted(int array[], int arraySize);
 
 int main() {
-    int nums[24] = {33, 18, 13, 3, 17, 14, 38, 21, 16, 40, 3, 18, 2, 5, 67, 34, 2, 67, 88, 94, 23, 63, 78, 80};
-    bottomUpMergeSort(nums, 24);
-    printNums(nums, 24);
+    int nums[23] = {18, 13, 3, 17, 14, 38, 21, 16, 40, 3, 18, 2, 5, 67, 34, 2, 67, 88, 94, 23, 63, 78, 80};
+    int numsSize = 23;
+    bottomUpMergeSort(nums, numsSize);
+    printNums(nums, numsSize);
+    std::string output;
+    isSorted(nums, numsSize) == true ? output = "the array is sorted" : output = "the array is not sorted";
+    std::cout << "\n" << output << std::endl;
 }
 
 void printNums(int array[], int arraySize) {
@@ -17,4 +22,13 @@ void printNums(int array[], int arraySize) {
         std::cout << array[i] << ", ";
     }
     std::cout << "]" << std::endl;
+}
+
+bool isSorted(int array[], int arraySize) {
+    for (int i = 0; i < arraySize; i++) {
+        if (array[i] < array[0]) {
+            return false;
+        }
+    }
+    return true;
 }
